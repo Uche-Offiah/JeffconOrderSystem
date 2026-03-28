1c856bb4d9406004d0bfe7da22095cdcc12d3be8
Uche Offiah <evaristusoffiah@gmail.com>
2026-03-28 13:03:12 -0400

    Added EF DbContext, some Interfaces and Services

--- OrderService.API/Program.cs
+++ OrderService.API/Program.cs
@@ -1,8 +1,15 @@
+using OrderService.Domain.Interfaces;
+using OrderService.Application.Interfaces;
+using OrderService.Application.Services;
+using OrderService.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddScoped<IOrderService, OrderServiceHandler>();
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
 
 var app = builder.Build();
 
---
+++ OrderService.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderService.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderService.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Order> Orders { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+            
+        }
+    }
+}
