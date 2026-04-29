d858e263bf564b195a10ce5f71a6dc4a2d353450
Uche Offiah <evaristusoffiah@gmail.com>
2026-04-28 23:54:29 -0400

    incomplete jwt implementation

--- OrderService.Application/Services/AuthService.cs
+++ OrderService.Application/Services/AuthService.cs
@@ -1,8 +1,11 @@
-﻿using OrderService.Application.DTOs;
+﻿using Microsoft.IdentityModel.Tokens;
+using OrderService.Application.DTOs;
 using OrderService.Application.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,6 +17,39 @@ namespace OrderService.Application.Services
         {
             // implement the codes here
             return string.Empty;
+        }
+
+        public string GenerateJWTToken(OrderServiceUser user)
+        {
+            var issuer = _configuration["Jwt:Issuer"];
+            var audience = _configuration["Jwt:Audience"];
+            var secret = Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]);
+            var expires = DateTime.Now.AddMinutes(10);
+
+            SecurityTokenDescriptor descriptor = new()
+            {
+                Issuer = issuer,
+                Audience = audience,
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                new("Id", user?.Id),
+                new("EmailAddress", user?.EmailAddress),
+                //new(ClaimTypes.Role, user.UserCategory.ToString())
+                }),
+                Expires = expires,
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(secret), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            SecurityToken token = tokenHandler.CreateToken(descriptor);
+            var jwtToken = tokenHandler.WriteToken(token);
+
+            response.Status = 200;
+            response.Success = true;
+            response.Message = "Success";
+            response.Data = jwtToken;
+
+            return response;
         }
     }
 }
